56af99dde1c3cb9c1d125795d93c84ae2e6ffa13
Jonas Grimfelt <grimen@gmail.com>
2009-09-10 19:34:50 +0200

    Using super instead of alias_method - alias_method not needed.

--- lib/delayed_job_mailer.rb
+++ lib/delayed_job_mailer.rb
@@ -4,19 +4,17 @@ module Delayed
     def self.included(base)
       base.class_eval do
         class << self
-          alias_method :orig_method_missing, :method_missing
           
           def method_missing(method_symbol, *params)
-            
             if ::Delayed::Mailer.excluded_environments &&
               ::Delayed::Mailer.excluded_environments.include?(::RAILS_ENV.to_sym)
-              return orig_method_missing(method_symbol, *params)
+              return super(method_symbol, *params)
             end
             
             case method_symbol.id2name
-            when /^deliver_([_a-z]\w*)\!/ then orig_method_missing(method_symbol, *params)
+            when /^deliver_([_a-z]\w*)\!/ then super(method_symbol, *params)
             when /^deliver_([_a-z]\w*)/ then self.send_later(method_symbol.to_s + '!', *params)
-            else orig_method_missing(method_symbol, *params)
+            else super(method_symbol, *params)
             end
           end
         end
