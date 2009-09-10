dcbe6c9c73dc46c659277f9984694b927bd89663
Jonas Grimfelt <grimen@gmail.com>
2009-09-10 19:28:41 +0200

    Make it possible to exclude certain environments, such as testing where delayed e-mails as a behavior can be more of a pain.

--- lib/delayed_job_mailer.rb
+++ lib/delayed_job_mailer.rb
@@ -1,11 +1,18 @@
 module Delayed
   module Mailer
+    
     def self.included(base)
       base.class_eval do
         class << self
           alias_method :orig_method_missing, :method_missing
-
+          
           def method_missing(method_symbol, *params)
+            
+            if ::Delayed::Mailer.excluded_environments &&
+              ::Delayed::Mailer.excluded_environments.include?(::RAILS_ENV.to_sym)
+              return orig_method_missing(method_symbol, *params)
+            end
+            
             case method_symbol.id2name
             when /^deliver_([_a-z]\w*)\!/ then orig_method_missing(method_symbol, *params)
             when /^deliver_([_a-z]\w*)/ then self.send_later(method_symbol.to_s + '!', *params)
@@ -14,6 +21,15 @@ module Delayed
           end
         end
       end
+      
+      def self.excluded_environments=(*environments)
+        @@excluded_environments = environments && environments.flatten.collect! { |env| env.to_sym }
+      end
+      
+      def self.excluded_environments
+        @@excluded_environments ||= []
+      end
     end
+    
   end
 end
--- spec/delayed_job_mailer_spec.rb
+++ spec/delayed_job_mailer_spec.rb
@@ -20,6 +20,11 @@ class AsynchTestMailer < ActionMailer::Base
 end
 
 describe AsynchTestMailer do
+  before do
+    # We need a default environment...
+    Object.const_set 'RAILS_ENV', 'test' unless defined?(::RAILS_ENV)
+  end
+  
   describe 'deliver_test_mail' do
     before(:each) do
       @emails = ActionMailer::Base.deliveries
@@ -27,18 +32,31 @@ describe AsynchTestMailer do
       @params = 'noreply@autoki.de', 'joe@doe.com'
       AsynchTestMailer.stub(:send_later)
     end
-
+    
     it 'should not deliver the email at this moment' do
       AsynchTestMailer.deliver_test_mail *@params
       @emails.size.should == 0
     end
-
+    
     it 'should send deliver action to delayed job list' do
       AsynchTestMailer.should_receive(:send_later).with('deliver_test_mail!', *@params)
       AsynchTestMailer.deliver_test_mail *@params
     end
+    
+    it 'should not send deliver action to delayed job list for environments where delayed job mailer is disabled' do
+      excluded_environments = [:cucumber, :foo, 'bar']
+      ::Delayed::Mailer.excluded_environments = excluded_environments
+      
+      excluded_environments.each do |env|
+        Object.send :remove_const, 'RAILS_ENV'
+        Object.const_set 'RAILS_ENV', env.to_s
+        
+        AsynchTestMailer.should_not_receive(:send_later).with('deliver_test_mail!', *@params)
+        AsynchTestMailer.deliver_test_mail *@params
+      end
+    end
   end
-
+  
   describe 'deliver_test_mail!' do
     it 'should deliver the mail' do
       emails = ActionMailer::Base.deliveries
