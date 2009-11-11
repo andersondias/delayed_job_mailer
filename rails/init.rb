5b617c1ff5e867f24fc15cbf25850fabf7c7e55f
Anderson Dias <andersondaraujo@gmail.com>
2009-11-11 12:04:05 -0300

    Fixing path on initializer

--- rails/init.rb
+++ rails/init.rb
@@ -1,2 +1,2 @@
 # coding: utf-8
-require File.expand_path(File.join(File.dirname(__FILE__), *%w(lib .. delayed_job_mailer))
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib', 'delayed_job_mailer'))
